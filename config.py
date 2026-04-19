acbe4b484b660e01e7c285637b356cc67ba7ac3c
Spencer York <spencer.t.york@gmail.com>
2026-04-18 23:05:15 -0400

    reset

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ ADMIN_PORT  = 5000
 MQTT_BROKER = "192.168.40.73"  # Pi3 runs mosquitto
 
 def get_cert_hash():
-    cert = open("/iot-trust/device.crt", "rb").read()
+    cert = open("/Blockchain-IoT/device.crt", "rb").read()
     return hashlib.sha256(cert).hexdigest()
 ADMIN_KEY = "96dd7c00b6b94f7e2d193ef0ec3df095572323d2c8819a6c7df2446fb8023433"
